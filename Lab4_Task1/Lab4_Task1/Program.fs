e648fdd7cdca212528650a0e0b0bd9881b7f98c1
NgRage <molot97@gmail.com>
2026-03-19 21:55:59 +0500

    Lab4

---
+++ Lab4_Task1/Lab4_Task1/Program.fs
@@ -0,0 +1,88 @@
+﻿open System
+
+type BinaryTree<'T> =
+    | Node of 'T * BinaryTree<'T> * BinaryTree<'T>
+    | Empty
+
+let rec readNatural () =
+    let input = Console.ReadLine()
+    let success, n = Int32.TryParse(input)
+
+    if success && n > 0 then
+        n
+    else
+        printf "Ошибка: Число не натуральное. Повторите ввод: "
+        readNatural ()
+
+let rec readChar () =
+    let input = Console.ReadLine()
+    let success, n = Char.TryParse(input)
+
+    if success then
+        n
+    else
+        printf "Ошибка: Введите ровно один символ: "
+        readChar ()
+
+let rec insert value tree =
+    match tree with
+    | Empty ->
+        Node(value, Empty, Empty)
+    | Node(v, left, right) ->
+        if value < v then
+            Node(v, insert value left, right)
+        elif value > v then
+            Node(v, left, insert value right)
+        else
+            tree
+
+//Инфиксный обход
+let rec printInOrder tree =
+    match tree with
+    | Empty -> ()
+    | Node(data, left, right) ->
+        printInOrder left
+        printfn "Node %O" data
+        printInOrder right
+
+let rec mapTree f tree =
+    match tree with
+    | Empty -> Empty
+    | Node(data, left, right) ->
+        Node(f data, mapTree f left, mapTree f right)
+
+let replaceLastChar (newChar: char) (str: string) =
+    if String.IsNullOrEmpty(str) then
+        str
+    else
+        str.Substring(0, str.Length - 1) + string newChar
+
+let generateTree n =
+    let rand = Random()
+    let chars = "abcdefghijklmnopqrstuvwxyz"
+    let randomString () =
+        String.init (rand.Next(3, 8)) (fun _ -> string chars.[rand.Next(chars.Length)])
+    
+    let rec build count tree =
+        if count = 0 then
+            tree
+        else
+            build (count - 1) (insert (randomString()) tree)
+    build n Empty
+
+[<EntryPoint>]
+let main argv =
+    printf "Введите количество элемнетов: "
+    let N = readNatural()
+
+    let myTree = generateTree N
+    printfn "Исходное дерево: "
+    printInOrder myTree
+
+    printfn "Введите символ для замены: "
+    let myChar = readChar()
+
+    let mappedTree = mapTree(replaceLastChar myChar) myTree
+    printfn "Дерево после замены последнего символа: "
+    printInOrder mappedTree
+    0
---
+++ Lab4_Task2/Lab4_Task2/Program.fs
@@ -0,0 +1,73 @@
+﻿open System
+
+type BinaryTree<'T> =
+    | Node of 'T * BinaryTree<'T> * BinaryTree<'T>
+    | Empty
+
+let rec insert value tree =
+    match tree with
+    | Empty ->
+        Node(value, Empty, Empty)
+    | Node(v, left, right) ->
+        if value < v then
+            Node(v, insert value left, right)
+        elif value > v then
+            Node(v, left, insert value right)
+        else
+            tree 
+
+let rec readNatural () =
+    let input = Console.ReadLine()
+    let success, n = Int32.TryParse(input)
+
+    if success && n > 0 then
+        n
+    else
+        printf "Ошибка: Число не натуральное. Повторите ввод: "
+        readNatural ()
+
+//Инфиксный обход
+let rec printInOrder tree =
+    match tree with
+    | Empty -> ()
+    | Node(data, left, right) ->
+        printInOrder left
+        printfn "Node %O" data
+        printInOrder right
+
+let rec foldTree f state tree =
+    match tree with
+    | Empty -> state
+    | Node(data, left, right) ->
+        let state1 = foldTree f state left
+        let state2 = f state1 data left right
+        foldTree f state2 right
+
+let sumLeaves tree =
+    foldTree (fun acc data left right ->
+        match left, right with
+        | Empty, Empty when data % 2 = 0 -> acc + data
+        | _ -> acc
+    ) 0 tree
+
+let generateTree n =
+    let rand = Random()
+    let rec build count tree =
+        if count = 0 then
+            tree
+        else
+            build (count - 1) (insert (rand.Next(1, 100)) tree)
+    build n Empty
+
+[<EntryPoint>]
+let main argv =
+    printf "Введите количество элементов: "
+    let N = readNatural()
+
+    let myTree = generateTree N
+    printfn "Исходное дерево: "
+    printInOrder myTree
+
+    let sum = sumLeaves myTree
+    printfn "Сумма четных значений в листьях: %i" sum
+    0
